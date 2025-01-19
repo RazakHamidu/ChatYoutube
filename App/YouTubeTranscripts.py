c425cd2308693952114925da3e105dfdf0585f4b
Razak Hamidu <51274420+RazakHamidu@users.noreply.github.com>
2025-01-19 18:35:49 +0100

    Update YouTubeTranscripts.py

--- App/YouTubeTranscripts.py
+++ App/YouTubeTranscripts.py
@@ -1,9 +1,13 @@
-from langchain_community.document_loaders import YoutubeLoader
+from langchain_community.document_loaders import YoutubeLoader  # Libreria per caricare trascrizioni da YouTube
 
+# Funzione per ottenere le trascrizioni di un video YouTube
 def YouTubeTranscripts(urlVideoYuotubr):
-    loader = YoutubeLoader.from_youtube_url(urlVideoYuotubr, language=["en", "it"],)
+    # Crea un'istanza del loader con la lingua desiderata
+    loader = YoutubeLoader.from_youtube_url(urlVideoYuotubr, language=["en", "it"])
 
+    # Carica le trascrizioni dal video
     documents = loader.load()
 
+    # Restituisce il contenuto della trascrizione del video
     for doc in documents: 
         return doc.page_content
