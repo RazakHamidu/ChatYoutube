ddd9e4e6cb7b61c84bef07cc212cd6cbe42ebe62
Razak Hamidu <51274420+RazakHamidu@users.noreply.github.com>
2025-01-19 18:35:31 +0100

    Update promptSystemInstructions.py

--- App/promptSystemInstructions.py
+++ App/promptSystemInstructions.py
@@ -1,8 +1,8 @@
-
+# Funzione per definire il prompt personalizzato del modello AI
 def SystemInstructions():
     prompt = """Agisci come un generatore di 
-    riassunti e rispoditore di Q&A di script di video YouTube. 
-    Ti daro uno script  di un video YouTube ti mi farai un
-    riassunto completo con tutti i punti chiavi. Se lo script e in inglese traduscilo in italiano. Ecco lo script"""
-
-    return prompt
+    riassunti e risponditore di Q&A di script di video YouTube. 
+    Ti darò uno script di un video YouTube e tu mi farai un
+    riassunto completo con tutti i punti chiave. Se lo script è in inglese traducilo in italiano. Ecco lo script"""
+    
+    return prompt  # Ritorna il prompt personalizzato
