6485d67ab66ceebbc811ff4069dbd7c44c4dd8c6
Razak Hamidu <razakgemer86@gmail.com>
2025-01-19 13:13:58 +0100

    Creare la UI di chat (demo)

--- backend/VideoSection.py
+++ backend/VideoSection.py
@@ -1,4 +1,5 @@
 import streamlit as st
 
-VIDEO_URL = "https://youtu.be/CNsvts6pVzo?si=nEUDKEQhUu6xaBqL"
-st.video(VIDEO_URL)
+def VideoSection(): 
+    VIDEO_URL = "https://youtu.be/CNsvts6pVzo?si=nEUDKEQhUu6xaBqL"
+    st.video(VIDEO_URL)
