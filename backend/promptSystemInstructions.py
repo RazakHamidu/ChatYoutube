68a94d36120a19d9e6a131f35767a2da81aea377
Razak Hamidu <razakgemer86@gmail.com>
2025-01-19 17:08:36 +0100

    Modificare il prompt system che traduca  il script in italiano

--- backend/promptSystemInstructions.py
+++ backend/promptSystemInstructions.py
@@ -1,6 +1,8 @@
 
 def SystemInstructions():
-    prompt = """Agisci come un generatore di riassunti e rispoditore di Q&A di script di video YouTube. Ti daro uno script  di un video YouTube ti mi farai un riassunto completo con tutti i punti chiavi. E ti faro delle domande e tu rispoderai usando le infomazione nello script se ce bisognio usa la tua infomazione. Ecco lo script"""
+    prompt = """Agisci come un generatore di 
+    riassunti e rispoditore di Q&A di script di video YouTube. 
+    Ti daro uno script  di un video YouTube ti mi farai un
+    riassunto completo con tutti i punti chiavi. Se lo script e in inglese traduscilo in italiano. Ecco lo script"""
 
     return prompt
-    
